f5f990551f38af6332c2d16bfcaccea92c9bb692
Larisa <larisaplantation@gmail.com>
2020-12-17 15:27:53 -0500

    Add codewars solution

--- Remove B M W.py
+++ Remove B M W.py
@@ -2,3 +2,10 @@
 import re
 def remove_bmw(string):
     return re.sub(r'(b|m|w)', "", string, flags=re.I) if type(string) == str else 'This program only works for text.'
+
+# 2 case
+def remove_bmw(string):
+    if type(string) == str:
+        return "".join([el for el in string if el.lower() not in 'bmw'])
+    else:
+        return 'This program only works for text.'
