97087f0f4b2896b14dbd3d6405d18a98199f18c6
Larisa <larisaplantation@gmail.com>
2021-01-03 13:20:01 -0500

    Add codewars solution

---
+++ Pairs of integers from 0 to n.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/588e27b7d1140d31cb000060
+def generate_pairs(n):
+    arr = []
+    for i in range(n+1):
+        for j in range(i,n+1):
+            arr.append([i,j])
+    return arr
