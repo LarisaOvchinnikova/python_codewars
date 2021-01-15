05f62bc47497d85fd2dbb2f6e941fa6d0f93cc70
Larisa <larisaplantation@gmail.com>
2021-01-15 14:23:35 -0500

    Add codewars solution

---
+++ Exclamation marks series 12 - Remove odd number continuous exclamation marks and question marks.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/57fb0f3f9610ced69000023c
+def remove(s):
+    for i in range(2):
+        t = ''
+        for el in s:
+            if t == '' or t[-1] == el:
+                t += el
+            else:
+                t += " " + el
+        t = t.split(' ')
+        s = [el for el in t if len(el) % 2 == 0 or len(el) == 1]
+        s = "".join(s)
+    return "".join(s)
