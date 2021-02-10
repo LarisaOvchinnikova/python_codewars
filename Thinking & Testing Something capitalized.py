bcf61109ab01780469a79069df6195d181faaa28
Larisa <larisaplantation@gmail.com>
2021-02-10 15:06:37 -0500

    Add codewars solution

--- Thinking & Testing Something capitalized.py
+++ Thinking & Testing Something capitalized.py
@@ -1,3 +1,3 @@
 # https://www.codewars.com/kata/56d93f249c844788bc000002
 def testit(s):
-    return "" if s == "" else " ".join([el[:-1]+ el[-1].upper() for el in s.split()])
+    return "" if s == "" else " ".join([el[:-1] + el[-1].upper() for el in s.split()])
