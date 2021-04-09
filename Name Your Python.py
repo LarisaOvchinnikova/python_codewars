b71c816163bd151be6b4d12370bbb1b558698f12
Larisa <larisaplantation@gmail.com>
2021-04-09 10:50:57 -0400

    Add codewars solution

---
+++ Name Your Python.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/53cf459503f9bbb774000003
+# Create a Python class that takes a name.
+# example usage: Python('Bubba')
+class Python:
+    def __init__(self, name):
+        self.name = name
